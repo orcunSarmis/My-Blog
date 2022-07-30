c180e9057eb2233be573a76823cc19cb51b7e113
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-30 21:38:39 +1000

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Welcome to my blog!</h1>
+    <p>
+        lorem nolsen
+    </p>
+    </>
+);
+
+export default HomePage;
+
